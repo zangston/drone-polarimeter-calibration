3e8f755ff988ba11f706b971620f6cbe4e0694e9
Winston Zhang <winston.y.zhang@gmail.com>
2024-11-27 19:32:37 -0500

    Complete setup program

--- zwo/zwo-setup/zwo-setup.cpp
+++ zwo/zwo-setup/zwo-setup.cpp
@@ -22,7 +22,7 @@ int main() {
     cout << "ZWO ASICamera test" << endl;
     
     long image_size = 0;
-    int image_bytes = 1;
+    int bytes_per_pixel = 1;
 
     // Read camera info, exit if no cameras are connected
     int connected_cameras = ASIGetNumOfConnectedCameras();
@@ -51,8 +51,94 @@ int main() {
     cout << "  Bit depth: " << camera_info.BitDepth << endl;
     cout << "  Trigger cam: " << (camera_info.IsTriggerCam ? "Yes" : "No") << endl;
 
-    // Calculate image size
+    // Calculate image size in bytes
+    int image_dimensions = camera_info.MaxWidth * camera_info.MaxHeight;
+    // Correct for monochromatic vs color image
+    if (camera_info.IsColorCam == ASI_FALSE)
+        bytes_per_pixel = (camera_info.BitDepth > 8) ? 2 : 1;
+    else
+        bytes_per_pixel = 3;
+    image_size = image_dimensions * bytes_per_pixel;
+    cout << "Image size: " << image_size << " bytes" << endl;
 
+    // Open and intialize camera
+    cout << "Opening camera" << endl;
+    if (ASIOpenCamera(camera_info.CameraID) != ASI_SUCCESS) {
+        cerr << "Error opening camera" << endl;
+        return 1;
+    }
+    cout << "Initalizing camera" << endl;
+    if (ASIInitCamera(camera_info.CameraID) != ASI_SUCCESS) {
+        cerr << "Error initializing camera" << endl;
+    }
+
+    // Get camera controls
+    int asi_num_controls = 0;
+    if (ASIGetNumOfControls(camera_info.CameraID, &asi_num_controls) != ASI_SUCCESS) {
+        cerr << "Error getting number of controls.\n";
+        return 1;
+    }
+    for (int i = 0; i < asi_num_controls; ++i) {
+        ASI_CONTROL_CAPS control_caps;
+        if (ASIGetControlCaps(camera_info.CameraID, i, &control_caps) == ASI_SUCCESS) {
+            cout << "  Property " << control_caps.Name << ": [" 
+                << control_caps.MinValue << ", " << control_caps.MaxValue 
+                << "] = " << control_caps.DefaultValue
+                << (control_caps.IsWritable ? " (set)" : "") 
+                << " - " << control_caps.Description << "\n";
+        }
+    }
+
+    // Exposure setup
+    cout << "Starting exposure" << endl;
+    ASI_EXPOSURE_STATUS asi_exp_status;
+    ASIGetExpStatus(camera_info.CameraID, &asi_exp_status);
+    if (asi_exp_status != ASI_EXP_IDLE) {
+        cerr << "Cannot start exposure if camera is not idle. Aborting..." << endl;
+        return 1;
+    }
+
+    // Set exposure time
+    long exposure_time = 20 * 1000000;   // 20 seconds
+    cout << "Set exposure time: " << exposure_time / 1000000 << " seconds" << endl;
+    if (ASISetControlValue(camera_info.CameraID, ASI_EXPOSURE, exposure_time, ASI_FALSE) != ASI_SUCCESS) {
+        cerr << "Error setting exposure time" << endl;
+        return 1;
+    }
+
+    // START THE EXPOSURE!!!!
+    if (ASIStartExposure(camera_info.CameraID, ASI_FALSE) != ASI_SUCCESS) {
+        cerr << "Error starting exposure" << endl;
+        return 1;
+    }
+
+    // Wait for camera to take exposure
+    while (true) {
+        ASIGetExpStatus(camera_info.CameraID, &asi_exp_status);
+        if (asi_exp_status == ASI_EXP_SUCCESS) {
+            cout << "Successfully took exposure" << endl;
+            break;
+        }
+        else if (asi_exp_status == ASI_EXP_FAILED) {
+            cerr << "Exposure capture failed" << endl;
+            return 1;
+        }
+    }
+
+    // Retrieve exposure data
+    vector<unsigned char> asi_image(image_size);
+    if (ASIGetDataAfterExp(camera_info.CameraID, asi_image.data(), image_size) != ASI_SUCCESS) {
+        cerr << "Error reading exposure data" << endl;
+        return 1;
+    }
+
+    // Print the first few bytes of exposure data
+    cout << "Image data (first 20 bytes): " << endl;
+    for (int i = 0; i < 20; i++)
+    {
+        cout << static_cast<int>(asi_image[i]) << " ";
+    }
+    cout << endl;
 
     // Close camera
     cout << "Closing camera" << endl;
