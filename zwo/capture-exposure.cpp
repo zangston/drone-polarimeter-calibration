99ca687ef8711b6753c0491e7f7bd7c17a7bdd7b
Winston Zhang <winston.y.zhang@gmail.com>
2025-02-24 11:49:26 -0500

    Add sdk-supported image saving

--- zwo/capture-exposure.cpp
+++ zwo/capture-exposure.cpp
@@ -3,9 +3,11 @@
 #include <vector>
 #include <ctime>
 #include <iomanip>
+#include <opencv2/opencv.hpp>
 #include "/home/declan/RPI/zwo/dependencies/zwo-asi-sdk/1.36/linux_sdk/include/ASICamera2.h"
 
 using namespace std;
+using namespace cv;
 
 int main(int argc, char *argv[]) {
     cout << "Starting ZWO ASICamera exposure..." << endl;
@@ -22,15 +24,16 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    int image_size = camera_info.MaxWidth * camera_info.MaxHeight;
+    int width = camera_info.MaxWidth;
+    int height = camera_info.MaxHeight;
     int bytes_per_pixel = (camera_info.BitDepth > 8) ? 2 : 1;
-    image_size *= bytes_per_pixel;
-    
+    int image_size = width * height * bytes_per_pixel;
+
     if (ASIOpenCamera(camera_info.CameraID) != ASI_SUCCESS || ASIInitCamera(camera_info.CameraID) != ASI_SUCCESS) {
         cerr << "Error initializing camera" << endl;
         return 1;
     }
-    
+
     double exposure_seconds = (argc > 1) ? stod(argv[1]) : 20;
     long exposure_time = exposure_seconds * 1000000;
     if (ASISetControlValue(camera_info.CameraID, ASI_EXPOSURE, exposure_time, ASI_FALSE) != ASI_SUCCESS) {
@@ -59,21 +62,27 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
+    // Convert to OpenCV Mat
+    Mat image;
+    if (bytes_per_pixel == 2) {
+        image = Mat(height, width, CV_16UC1, asi_image.data());
+    } else {
+        image = Mat(height, width, CV_8UC1, asi_image.data());
+    }
+
+    // Save with timestamp
     time_t now = time(0);
     tm *ltm = localtime(&now);
     stringstream filename;
     filename << "exposure-" << 1900 + ltm->tm_year << setfill('0') << setw(2) << 1 + ltm->tm_mon
              << setw(2) << ltm->tm_mday << "-" << setw(2) << ltm->tm_hour << setw(2) << ltm->tm_min
-             << setw(2) << ltm->tm_sec << ".bin";
+             << setw(2) << ltm->tm_sec << ".png";
 
-    ofstream output_file(filename.str(), ios::binary);
-    if (!output_file) {
-        cerr << "Error opening file for writing" << endl;
+    if (!imwrite(filename.str(), image)) {
+        cerr << "Error saving image" << endl;
         return 1;
     }
-    output_file.write(reinterpret_cast<char*>(asi_image.data()), asi_image.size());
-    output_file.close();
 
-    cout << "Exposure data saved to " << filename.str() << endl;
+    cout << "Exposure image saved to " << filename.str() << endl;
     return 0;
 }
