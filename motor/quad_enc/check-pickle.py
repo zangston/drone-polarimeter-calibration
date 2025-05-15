0b02d919fdd9af1b0b282e4675f96f4794eceed1
Winston Zhang <winston.y.zhang@gmail.com>
2025-05-15 16:38:43 -0400

    check-pickle.py: parameterize pkl file argument, refactor to work over SSH

--- motor/quad_enc/check-pickle.py
+++ motor/quad_enc/check-pickle.py
@@ -1,9 +1,16 @@
 import pickle
 import matplotlib.pyplot as plt
+import sys
+import os
 
-f = open("/home/declan/RPI/readout/quad_enc/nerd_file2.pkl", "rb")           #change this!!!!!!!!!!!!!!!!!!!!!!!!
-spacetime = pickle.load(f)          #{time : radians}
-f.close()
+if len(sys.argv) < 2:
+    print("Usage: python script.py <path_to_pkl_file>")
+    sys.exit(1)
+
+pkl_file = sys.argv[1]
+
+with open(pkl_file, "rb") as f:
+    spacetime = pickle.load(f)  # {time : radians}
 
 loc = []
 time = []
@@ -11,26 +18,24 @@ time = []
 old_key = 0
 old_val = 0
 zero = next(iter(spacetime.keys()))
-print("Starting time [millisec]: ", zero)
+print("Starting time [millisec]:", zero)
+
 for key, val in spacetime.items():
     if val > 0.000001:
         if old_val != val and old_key != key:
             loc.append(val)
-            seconds = key/1000.0                # millisec to sec 
-            time.append(seconds-(zero/1000.0))
+            seconds = key / 1000.0
+            time.append(seconds - (zero / 1000.0))
             old_key = key
             old_val = val
-        else:
-            continue
-    else:
-        continue
 
-
-# print(spacetime)
-# print(loc)
-# print(time)
 plt.figure()
 plt.ylabel("Motor Position [Radians]")
 plt.xlabel("Time [seconds]")
 plt.scatter(time, loc)
-plt.show()
+
+# Save plot to PNG in same directory as the .pkl file
+base_name = os.path.splitext(os.path.basename(pkl_file))[0]
+output_path = os.path.join(os.path.dirname(pkl_file), f"{base_name}_plot.png")
+plt.savefig(output_path)
+print(f"Plot saved to {output_path}")
